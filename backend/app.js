576f6bc855c1f6f203fe7c0e6c99417f35895561
Hayden Bradfield <hbradfield20@gmail.com>
2020-03-19 09:38:41 -0700

    renamed route files to make distinctive between general account and business account

--- backend/app.js
+++ backend/app.js
@@ -7,8 +7,8 @@ app.use(express.json());
 app.use(fileupload());
 
 //Routes
-app.use('/api/v1/auth', require('./routes/auth'));
-app.use('/api/v1/account', require('./routes/account'));
+app.use('/api/v1/auth', require('./routes/generalAcctAuth'));
+app.use('/api/v1/account', require('./routes/generalAccount'));
 
 
 app.listen(config.port, () => console.log(`Listening on port ${config.port}`));
