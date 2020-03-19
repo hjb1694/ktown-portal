59db0d7ee61a1b7a401872c7affc7c35b95f3d95
Hayden Bradfield <hbradfield20@gmail.com>
2020-03-19 14:18:30 -0700

    created migration for business anouncements

---
+++ backend/database/migrations/20200319141121_create_business_announcements_table.js
@@ -0,0 +1,17 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('business_announcements', table => {
+      table.increments('id');
+      table.bigInteger('business_listing_id').notNullable()
+      .references('id').inTable('business_listings');
+      table.string('headline', 50).notNullable();
+      table.text('details');
+      table.integer('posting_status').notNullable()
+      .references('id').inTable('posting_statuses');
+      table.timestamps();
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.removeTable('business_announcements');
+};
