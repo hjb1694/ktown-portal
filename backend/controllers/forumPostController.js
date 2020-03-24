c298053d68772e6f623a01a7b2274e49db7b0815
Hayden Bradfield <hbradfield20@gmail.com>
2020-03-23 23:00:05 -0700

    worked on new forum post controller

--- backend/controllers/forumPostController.js
+++ backend/controllers/forumPostController.js
@@ -1,9 +1,13 @@
 const {validationResult} = require('express-validator');
 const isBase64 = require('is-base64');
+const {promisify} = require('util');
 const {
     sanitizeForumPost
 } = require('../utils/helpers');
 const JSDOM = require('jsdom').JSDOM;
+const base64Img = require('base64-img');
+const toImage = promisify(base64Img.img);
+const uuid = require('uuid').v4;
 
 exports.createNewForumPost = async (req,res) => {
 
@@ -24,6 +28,7 @@ exports.createNewForumPost = async (req,res) => {
     } = req.body;
 
     errors = [];
+    const srcs = [];
 
     const sanitizedContent = sanitizeForumPost(content);
 
@@ -32,12 +37,10 @@ exports.createNewForumPost = async (req,res) => {
     const imgElems = document.getElementsByTagName('img');
 
 
-    if(imgElems.length > 3)
-        errors.push({msg : 'Only up to 3 images are allowed.'});
+    if(imgElems.length > 2)
+        errors.push({msg : 'Only up to 2 images are allowed.'});
 
     if(imgElems.length){
-
-        const srcs = [];
 
         for(let i = 0; i < imgElems.length; i++){
 
@@ -49,7 +52,7 @@ exports.createNewForumPost = async (req,res) => {
             const allIsBase64 = srcs.every(item => isBase64(item, {mimeRequired : true}));
 
             if(!allIsBase64)
-                errors.push({msg : 'Images must be base64 format.'});
+                errors.push({msg : 'Images must be base64.'});
                     
         }
     }
@@ -62,8 +65,28 @@ exports.createNewForumPost = async (req,res) => {
             }
         });
 
+    try{   
+        let mainImagePath;
 
-    res.send('Create new forum post controller');
+        if(srcs.length){
+
+            mainImagePath = `${Date.now()}-${uuid()}`;
+
+            await toImage(srcs[0], './public/uploads', mainImagePath);
+
+        }
+
+        res.send('Create new forum post controller');
+    }catch(e){
+        console.log(e);
+        res.status(500).json({
+            status : 'error', 
+            data : {
+                msg : 'An server error has occurred.'
+            }
+        });
+    }
+
 
 
 }
